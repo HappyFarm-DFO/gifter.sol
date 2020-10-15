8a8c7d1c93ae290ef3a368117ec02b1674b24b64
maxxflyer <maxxflyer@gmail.com>
2020-10-15 11:18:19 +0200

    Create InstaClaimer_1_3.sol

---
+++ InstaClaimer_1_3.sol
@@ -0,0 +1,21 @@
+contract InstaClaimer{
+    
+    PeriodicStaker public staker;
+    uint public tot;
+    mapping(address => bool) rewarded;
+    
+    constructor() public{
+        staker=PeriodicStaker(0x7d410AFA45377006A0F79Ae6157A6A873Bfa5567);
+        tot=staker.total_stake();
+    }
+ 
+    function instaClaim() public returns(bool){
+        require(staker.status()==2);
+        require(staker.stake(msg.sender)>0);
+        require(!rewarded[msg.sender]);
+        rewarded[msg.sender]=true;
+        require(ERC20(0x801F90f81786dC72B4b9d51Ab613fbe99e5E4cCD).transfer(msg.sender, 30000000000000000000/(staker.total_stake()/staker.stake(msg.sender))));
+        return true;
+    } 
+    
+}
